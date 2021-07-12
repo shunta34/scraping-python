8c4b4c0206d14920df35bcbcd5dad1e25d43c3cc
shunta <s.goto@kaoiri.co.jp>
2021-07-12 22:59:11 +0900

    finished scraping11

---
+++ scraping10.py
@@ -0,0 +1,17 @@
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+
+# chrome_path = '/Users/gotou/individual/public/scraping-python/chromedriver'
+
+# options = Options()
+# options.add_argument('--incognito')
+
+# driver = webdriver.Chrome(executable_path = chrome_path, options = options)
+
+# url = 'https://tech-diary.net'
+# driver.get(url)
+
+# sleep(5)
+
+# driver.quit()
--- scraping9.py
+++ scraping9.py
@@ -1,56 +1,56 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-from time import sleep
+# import requests
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# from time import sleep
 
-url = 'https://suumo.jp/chintai/tokyo/sc_shinjuku/?page={}'
-d_list = []
+# url = 'https://suumo.jp/chintai/tokyo/sc_shinjuku/?page={}'
+# d_list = []
 
-for i in range(1, 3):
-  print('d_listの大きさ :', len(d_list))
-  target_url = url.format(i)
-  print(target_url)
+# for i in range(1, 3):
+#   print('d_listの大きさ :', len(d_list))
+#   target_url = url.format(i)
+#   print(target_url)
 
-  r = requests.get(target_url)
-  sleep(1)
-  soup = BeautifulSoup(r.text)
+#   r = requests.get(target_url)
+#   sleep(1)
+#   soup = BeautifulSoup(r.text)
 
 
-  contents = soup.find_all('div', class_ = 'cassetteitem')
+#   contents = soup.find_all('div', class_ = 'cassetteitem')
 
-  for content in contents:
-    detail = content.find('div', class_ = 'cassetteitem-detail')
-    table = content.find('table', class_ = 'cassetteitem_other')
-    title = detail.find('div', class_ = 'cassetteitem_content-title').text
-    address = detail.find('li', class_ = 'cassetteitem_detail-col1').text
-    access = detail.find('li', class_ = 'cassetteitem_detail-col2').text
-    age = detail.find('li', class_ = 'cassetteitem_detail-col3').text
-    tr_tags = table.find_all('tr', class_ = 'js-cassette_link')
+#   for content in contents:
+#     detail = content.find('div', class_ = 'cassetteitem-detail')
+#     table = content.find('table', class_ = 'cassetteitem_other')
+#     title = detail.find('div', class_ = 'cassetteitem_content-title').text
+#     address = detail.find('li', class_ = 'cassetteitem_detail-col1').text
+#     access = detail.find('li', class_ = 'cassetteitem_detail-col2').text
+#     age = detail.find('li', class_ = 'cassetteitem_detail-col3').text
+#     tr_tags = table.find_all('tr', class_ = 'js-cassette_link')
 
-    for tr_tag in tr_tags:
-      floor, price, first_fee, capacity = tr_tag.find_all('td')[2:6]
-      fee, management_fee = price.find_all('li')
-      deposit, gratuity = first_fee.find_all('li')
-      madori, menseki = capacity.find_all('li')
-      d = {
-        'title': title,
-        'address': address,
-        'access': access,
-        'age': age,
-        'floor': floor.text,
-        'fee': fee.text,
-        'management_fee': management_fee.text,
-        'deposit': deposit.text,
-        'gratuity': gratuity.text,
-        'madori': madori.text,
-        'menseki': menseki.text
-      }
-      d_list.append(d)
-print(d_list[:2])
+#     for tr_tag in tr_tags:
+#       floor, price, first_fee, capacity = tr_tag.find_all('td')[2:6]
+#       fee, management_fee = price.find_all('li')
+#       deposit, gratuity = first_fee.find_all('li')
+#       madori, menseki = capacity.find_all('li')
+#       d = {
+#         'title': title,
+#         'address': address,
+#         'access': access,
+#         'age': age,
+#         'floor': floor.text,
+#         'fee': fee.text,
+#         'management_fee': management_fee.text,
+#         'deposit': deposit.text,
+#         'gratuity': gratuity.text,
+#         'madori': madori.text,
+#         'menseki': menseki.text
+#       }
+#       d_list.append(d)
+# print(d_list[:2])
 
-df = pd.DataFrame(d_list)
+# df = pd.DataFrame(d_list)
 # print(df.head())
 # print(df.shape)
-print(df.title.unique())
-print(len(df.title.unique()))
-df.to_csv('test.csv', index = None, encoding = 'utf-8-sig')
+# print(df.title.unique())
+# print(len(df.title.unique()))
+# df.to_csv('test.csv', index = None, encoding = 'utf-8-sig')
